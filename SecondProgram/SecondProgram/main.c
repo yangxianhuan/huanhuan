02a1daefb0a950dd3534ec8212626498ea76dfc2
lkjy <yang@lkjydemac-mini-5.local>
2016-01-08 18:12:51 +0800

    啊，我的第一次提交

---
+++ SecondProgram/SecondProgram/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SecondProgram
+//
+//  Created by YANG on 16/1/8.
+//  Copyright © 2016年 杨先环. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
